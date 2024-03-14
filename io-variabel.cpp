faabcf0bb3940c099dbcace0096d23b95ab275b2
mnmuzaki <142678527+mnmuzaki@users.noreply.github.com>
2024-03-15 04:44:46 +0700

    Create io-variabel.cpp
    program input/output, tipe data dan variabel

---
+++ io-variabel.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+  string nama ="Budi";
+  int usia = 20;
+  float bobot = 60.5;
+  char gender = 'L';
+  bool merokok = 0;
+
+  cout<<nama<<endl;
+  cout<<usia<<endl;
+  cout<<bobot<<endl;
+  cout<<gender<<endl;
+  cout<<merokok<<endl;
+
+  return 0;
+}
