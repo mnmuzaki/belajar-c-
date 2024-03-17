f46f0e483fc2153a9936720465f0dd05d52e6e67
mnmuzaki <m.n.muzaki@gmail.com>
2024-03-17 20:38:03 +0700

    commit program variabel

---
+++ variabel.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//deklarasi variabel
+	string nama = "bagong"; 	//deklarasi tipe string/kata
+	float bobot = 80.5; 		//deklarasi tipe float/bilangan desimal
+	char gender = 'L';			//deklarasi tipe char/ karakter
+	bool hobi_mancing = true;	//deklarasi tipe boolean
+	int usia = 19 ;				//deklarasi tipe integer
+	
+	//menampilkan data
+	cout<<"nama saya    : "<<nama<<endl;
+	cout<<"bobot saya   : "<<bobot<<endl;
+	cout<<"gender saya  : "<<gender<<endl;
+	cout<<"hobi mancing : "<<hobi_mancing<<endl;
+	cout<<"usia saya    : "<<usia<<endl;
+	
+	
+	
+	
+	
+	
+	//komentar 1 baris
+	/*
+	cout<<"masukkan usia anda : ";
+	cin>>usia;
+	
+	cout<<"usia anda adalah : "<<usia<<" tahun"<<endl;
+	*/	
+	return 0;
+}
